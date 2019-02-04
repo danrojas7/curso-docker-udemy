1d6a75cbeb7bf80c70a0309998352394f65acd8c
Your Name <you@example.com>
2019-02-03 23:22:52 -0500

    Se realiza actualización de los archivos de acuerdo a lo visto en el curso de docker en entornos de desarrollo de udemy

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,16 @@ use Illuminate\Http\Request;
 
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
 
 Route::get('/email', function (Request $request) {
-    $email = App\Email::updateOrCreate($request->all());
-    return sprintf('Thanks for submitting your email, %s! <a href="/">home</a>', $email->email);
+	$email = App\Email::updateOrCreate($request->all());
+
+	\Mail::raw('You have been subscribed', function($message) use($email){
+		$message->to($email->email);
+	});
+	return sprintf('Thanks for submitting your email, %s! <a href="/">home</a>', $email->email);
 });
 
 ?>
